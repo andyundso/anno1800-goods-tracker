f631beea13c831697db62e58e7f885d21854d055
Andy Pfister <andy.pfister@hey.com>
2022-02-19 05:37:31 -0800

    Install cocoon

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tom-select" # @2.0.0
+pin "@oddcamp/cocoon-vanilla-js", to: "@oddcamp--cocoon-vanilla-js.js" # @1.1.3
