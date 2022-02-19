af6f802fca5c50e316705f11d97ead95352328ca
Andy Pfister <andy.pfister@hey.com>
2022-02-19 11:18:30 -0800

    Add good exports

---
+++ db/migrate/20220219181933_add_exports.rb
@@ -0,0 +1,16 @@
+class AddExports < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exports, id: :uuid do |t|
+      t.decimal :quantity
+      t.references :local_produced_good, type: :uuid, null: false, foreign_key: true, index: true
+      t.references :island, type: :uuid, null: false, foreign_key: true, index: true
+
+      t.timestamps
+    end
+
+    change_table :available_goods, bulk: true do |t|
+      t.decimal :export
+      t.decimal :import
+    end
+  end
+end
