7e92ff0bffce33511c08eb2d65f7fff497746b02
Andy Pfister <andy.pfister@hey.com>
2022-02-20 02:00:58 -0800

    Replace importmaps-rails with esbuild

---
+++ config/initializers/assets.rb
@@ -0,0 +1,5 @@
+# Remix Icons
+Rails.application.config.assets.paths << Rails.root.join("node_modules/remixicon/fonts")
+
+# Tom Select
+Rails.application.config.assets.paths << Rails.root.join("node_modules/tom-select/dist/css")
