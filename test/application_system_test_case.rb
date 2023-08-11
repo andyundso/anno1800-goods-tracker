3c5ee347790a7d9d836e77271963fd02a8cbc7f2
Andy Pfister <andy.pfister@hey.com>
2023-08-11 18:16:57 +0200

    Ensure system tests can use Tom Select fields again

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 require "capybara-screenshot/minitest"
+require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include ActionView::RecordIdentifier
   include Capybara::Screenshot::MiniTestPlugin
+
+  TOM_SELECT_CONTROL_SELECTOR = ".ts-control"
+  TOM_SELECT_INPUT_SELECTOR = "#{TOM_SELECT_CONTROL_SELECTOR} input"
+  TOM_SELECT_OPTION_SELECTOR = ".ts-dropdown .ts-dropdown-content .option"
 
   # https://github.com/rubycdp/cuprite/issues/180#issuecomment-1004943531
   Capybara.register_driver(:anno_cuprite) do |app|
@@ -27,15 +32,27 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Ensure 404 bring up error pages
   Rails.application.config.action_dispatch.show_exceptions = true
 
-  def fill_in_tom_select_field(container, value)
-    within(container, match: :first) do
-      find(".ts-control").click
+  def fill_in_tom_select_field(value)
+    assert_selector(TOM_SELECT_CONTROL_SELECTOR, count: 1)
 
-      input_control = find(".ts-control input")
-      input_control.native.clear
-      input_control.send_keys(value)
+    find(TOM_SELECT_CONTROL_SELECTOR).click
+
+    assert_selector(".ts-dropdown", count: 1)
+
+    text_option_regex = /^#{Regexp.quote(value)}$/i
+    find(TOM_SELECT_CONTROL_SELECTOR).send_keys(:backspace, value)
+
+    assert_selector TOM_SELECT_OPTION_SELECTOR, text: text_option_regex, count: 1
+
+    find(TOM_SELECT_OPTION_SELECTOR, text: text_option_regex).click
+
+    within ".ts-control" do
+      assert_text value
     end
 
-    all(".ts-dropdown .ts-dropdown-content .option", text: /#{Regexp.quote(value)}/i)[0].click
+    current_page_context = "document.evaluate(\"#{current_scope.path}\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue"
+    page.execute_script %{#{current_page_context}.querySelector('#{TOM_SELECT_INPUT_SELECTOR}').blur() }
+
+    assert_no_selector ".ts-dropdown", text: value, count: 1
   end
 end
